079b25cfaf842e9a17300d64bca4010916ebe7e9
ddzelal <dzelal_d@live.com>
2024-12-01 18:56:08 +0100

    initial commit

---
+++ src/services.js
@@ -0,0 +1,16 @@
+const ourServices = {
+    'flash-call': {
+        service_id: 'flash-call',
+        unit_price: 0.05
+    },
+    'otp-call': {
+        service_id: 'otp-call',
+        unit_price: 0.03
+    },
+    'text-to-speech': {
+        service_id: 'text-to-speech',
+        unit_price: 0.04
+    }
+};
+
+module.exports = ourServices;
